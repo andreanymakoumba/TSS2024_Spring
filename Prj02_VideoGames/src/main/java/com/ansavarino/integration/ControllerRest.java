97628e2c9fc6e75fff6da9613717ebb2c642aa94
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-03-04 17:49:29 +0100

    Update ControllerRest.java

--- Prj02_VideoGames/src/main/java/com/ansavarino/integration/ControllerRest.java
+++ Prj02_VideoGames/src/main/java/com/ansavarino/integration/ControllerRest.java
@@ -24,6 +24,43 @@ public class ControllerRest {
 	public List<Gioco> getGiochi(){
 		return service.getGiochi();
 	}
+	
+	@CrossOrigin()
+	@GetMapping("/api/giochi/year")
+	public List<Gioco> getGiochiByYear(@PathVariable int year){
+		return service.getGiochi()
+				.stream()
+				.filter(g->g.getYear()==year)
+				.toList();
+	}
+	
+	@CrossOrigin()
+	@GetMapping("/api/giochi/genre/{genre}")
+	public List<Gioco> getGiochiByGenre(@PathVariable String genre){
+		return service.getGiochi()
+				.stream()
+				.filter(g->g.getGenre().contains(genre))
+				.toList();
+	}
+	
+	@CrossOrigin()
+	@GetMapping("/api/giochi/title/{title}")
+	public List<Gioco> getGiochiByTitle(@PathVariable String title){
+		return service.getGiochi()
+				.stream()
+				.filter(g->g.getTitle().contains(title))
+				.toList();
+	}
+	
+	@CrossOrigin()
+	@GetMapping("/api/giochi/platform/{platform}")
+	public List<Gioco> getGiochiByPlatform(@PathVariable String platform){
+		return service.getGiochi()
+				.stream()
+				.filter(g->g.getOriginalPlatform().contains(platform))
+				.toList();
+	}
+	
 	@GetMapping("/api/giochi/{id}")
 	public Gioco getGiochiById(@PathVariable int id){
 		return service.getGiochi()
